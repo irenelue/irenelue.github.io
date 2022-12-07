cb5376b402dee7764b8f3f9d26db3f930ea67ea8
irenelue <ilue@ucsd.edu>
2022-12-07 12:24:18 -0800

    updated password box customization

--- assets/js/password.js
+++ assets/js/password.js
@@ -1,13 +1,17 @@
 var password = "7E83tR?";
+const errorText = document.querySelector(".error-text");
+const btn = document.querySelector("button");
+
 
 function passcheck() {
     if (document.getElementById('pass1').value != password) {
-        alert('Whoops, incorrect password. Please try again.');
+        alert('Whoops, incorrect password. Please try again or contact me if you believe this is an error.');
         return false;
     }
 
     if (document.getElementById('pass1').value == password) {
-        alert('Success! Please close this box to enter.');
+        // alert('Success! Please close this box to enter.');
+        return;
     }
 }
     
