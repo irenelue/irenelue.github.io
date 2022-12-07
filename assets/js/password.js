d03267585a989ff511e6c76ac6ae0226ca097f4e
irenelue <ilue@ucsd.edu>
2022-12-07 00:32:14 -0800

    pw protected page done

--- assets/js/password.js
+++ assets/js/password.js
@@ -2,12 +2,12 @@ var password = "12345";
 
 function passcheck() {
     if (document.getElementById('pass1').value != password) {
-        alert('Whoops, incorrect password. Pleasery again.');
+        alert('Whoops, incorrect password. Please try again.');
         return false;
     }
 
     if (document.getElementById('pass1').value == password) {
-        alert('Correct password! Close this box to enter.');
+        alert('Success! Please close this box to enter.');
     }
 }
     
